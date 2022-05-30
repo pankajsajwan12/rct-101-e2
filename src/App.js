b9096eff6f08f071614446e0679e882fbd65869d
pankajsajwan12 <75352393+pankajsajwan12@users.noreply.github.com>
2022-05-30 14:37:30 +0530

    doing work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const App = () => {
-  return <div>{/* TODO: Code here */}</div>;
+  return <div>{/* TODO: Code here */}
+  <h3>Hello , I am from App.js</h3>
+  </div>;
 };
 
 export default App;
